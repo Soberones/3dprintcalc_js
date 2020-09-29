f512daa4b499094655eecf4f0125a3b7d9b1bc53
Soberones <45145126+Soberones@users.noreply.github.com>
2020-09-29 18:22:02 +0300

    math

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,8 @@ save.onclick = function () {
   let depreciationPrice = Number(
     document.getElementById("depreciationPrice").value
   );
+
+  let spentMaterial = Number(document.getElementById("spentMaterial").value);
   localStorage.setItem("delivery", delivery);
   localStorage.setItem("spoolWeight", spoolWeight);
   localStorage.setItem("spoolPrice", spoolPrice);
@@ -18,8 +20,7 @@ save.onclick = function () {
   localStorage.setItem("printingPrice", printingPrice);
   localStorage.setItem("postPrice", postPrice);
   localStorage.setItem("depreciationPrice", depreciationPrice);
-  ocalStorage.setItem("powerPrinter", powerPrinter);
-  alert("test");
+  localStorage.setItem("powerPrinter", powerPrinter);
 };
 load.onclick = function () {
   document.getElementById("delivery").value = localStorage.getItem("delivery");
@@ -44,4 +45,13 @@ load.onclick = function () {
   document.getElementById("depreciationPrice").value = localStorage.getItem(
     "depreciationPrice"
   );
+};
+
+calculate.onclick = function () {
+  const k = Number(1000);
+  let materialSum = (spoolPrice.value + delivery.value) / spoolWeight.value;
+  let materialSumValue = materialSum / (k * spentMaterial.value);
+
+  console.log(materialSumValue);
+  document.getElementById("testss").innerHTML = materialSumValue;
 };
