cb8bfcef952b2a5cc9dd9723ad00862df2a59bbd
Soberones <45145126+Soberones@users.noreply.github.com>
2020-09-30 17:57:25 +0300

    add pwa

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,42 @@
+if (navigator.serviceWorker.controller) {
+  console.log("[PWA Builder] active service worker found, no need to register");
+} else {
+  navigator.serviceWorker
+    .register("sw.js", {
+      scope: "../",
+    })
+    .then(function (reg) {
+      console.log("Service worker has been registered for scope:" + reg.scope);
+    });
+}
+
+window.onload = function () {
+  document.getElementById("spoolWeight").value = localStorage.getItem(
+    "spoolWeight"
+  );
+  document.getElementById("spoolPrice").value = localStorage.getItem(
+    "spoolPrice"
+  );
+  document.getElementById("electricPrice").value = localStorage.getItem(
+    "electricPrice"
+  );
+  document.getElementById("modelingPrice").value = localStorage.getItem(
+    "modelingPrice"
+  );
+  document.getElementById("printingPrice").value = localStorage.getItem(
+    "printingPrice"
+  );
+  document.getElementById("powerPrinter").value = localStorage.getItem(
+    "powerPrinter"
+  );
+  document.getElementById("postPrice").value = localStorage.getItem(
+    "postPrice"
+  );
+  document.getElementById("depreciationPrice").value = localStorage.getItem(
+    "depreciationPrice"
+  );
+};
+
 save.onclick = function () {
   // let delivery = Number(document.getElementById("delivery").value);
   let spoolWeight = Number(document.getElementById("spoolWeight").value);
